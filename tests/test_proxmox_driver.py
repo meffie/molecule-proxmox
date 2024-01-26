e5193bc48cd9393540abd786c5c8166e92189b9f
Michael Meffie <mmeffie@sinenomine.net>
2024-01-26 14:29:35 -0500

    Check for missing proxmox_secrets file
    The task which loads the proxmox_secrets file is set to avoid logging
    to avoid revealing the passwords. This makes troubleshooting a bit
    harder. So add an extra task to verify the given path exists before
    trying to load it.

--- tests/test_proxmox_driver.py
+++ tests/test_proxmox_driver.py
@@ -9,6 +9,9 @@ import subprocess
 import pytest
 
 
+PROJECT_DIR = pathlib.Path(__file__).resolve().parent / 'proxmox_driver'
+
+
 @contextlib.contextmanager
 def chdir(path):
     prev = os.getcwd()
@@ -19,17 +22,17 @@ def chdir(path):
         os.chdir(prev)
 
 
-def molecule(command, *args):
+def molecule(command, *args, expected_rc=0):
     args = ['molecule', command] + list(args)
     proc = subprocess.Popen(args)
     rc = proc.wait()
-    assert rc == 0
+    assert rc == expected_rc
 
 
 def check_env(name):
     """Verify test environment is set."""
     value = os.environ.get(name)
-    assert value != None, f'{name} is not set'
+    assert value is not None, f'{name} is not set'
     assert value != '', f'{name} is empty'
 
 
@@ -57,8 +60,52 @@ def test_molecule_test(scenario):
     check_env('PROXMOX_TOKEN_ID')
     check_env('PROXMOX_TOKEN_SECRET')
     check_env('PROXMOX_USER')
-    testdir = pathlib.Path(__file__).resolve().parent
-    projectdir = testdir / 'proxmox_driver'
-    with chdir(projectdir):
+    with chdir(PROJECT_DIR):
         molecule('reset', '--scenario-name', scenario)
         molecule('test', '--scenario-name', scenario)
+
+
+def opener(path, flags):
+    """ Create the file with mode bits set to 600. """
+    return os.open(path, flags, 0o600)
+
+
+@pytest.fixture
+def secrets():
+    check_env('PROXMOX_HOST')
+    check_env('PROXMOX_USER')
+    check_env('PROXMOX_PASSWORD')
+    proxmox_host = os.environ['PROXMOX_HOST']
+    proxmox_user = os.environ['PROXMOX_USER']
+    proxmox_password = os.environ['PROXMOX_PASSWORD']
+    path = pathlib.Path('.secrets.yaml').absolute()
+    with open(path, 'w', opener=opener) as f:
+        f.write('---\n')
+        f.write(f'api_host: {proxmox_host}\n')
+        f.write(f'api_user: {proxmox_user}\n')
+        f.write(f'api_password: {proxmox_password}\n')
+    os.environ['PROXMOX_SECRETS'] = path.as_posix()
+    yield path
+    os.unlink(path)
+
+
+def test_secrets_file_present(secrets):
+    print("")
+    check_env('PROXMOX_NODE')
+    check_env('PROXMOX_SSH_IDENTITY_FILE')
+    check_env('PROXMOX_SSH_USER')
+    check_env('PROXMOX_TEMPLATE_NAME')
+    with chdir(PROJECT_DIR):
+        molecule('reset', '--scenario-name', 'secrets')
+        molecule('test', '--scenario-name', 'secrets')
+
+
+def test_secrets_file_missing():
+    print("")
+    check_env('PROXMOX_NODE')
+    check_env('PROXMOX_SSH_IDENTITY_FILE')
+    check_env('PROXMOX_SSH_USER')
+    check_env('PROXMOX_TEMPLATE_NAME')
+    with chdir(PROJECT_DIR):
+        molecule('reset', '--scenario-name', 'secrets')
+        molecule('create', '--scenario-name', 'secrets', expected_rc=2)
