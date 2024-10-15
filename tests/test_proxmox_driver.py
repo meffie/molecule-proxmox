d36c9c11285b64e56c86a3b8c4fda7cfdc9a94e3
Michael Meffie <mmeffie@sinenomine.net>
2024-10-15 08:24:13 -0400

    Proxmox secrets script (#20)
    * Rename unit test environment variables
    
    The proxmox ansible module reads several environment variables that
    start with the prefix PROXMOX_.  Rename our environment variables used
    in the unit test scenarios to avoid clashing with the ones in the
    module.
    
    * Introduce debug driver option
    
    Add a new option to disable the "no_log" directives on tasks which load
    senstive information.  This can be useful when troubleshooting a new
    setup.
    
    * Run proxmox_secrets when it is executable
    
    When the proxmox_secrets option specifies an executable file, run that
    file and capture the stdout as yaml to be used for driver settings.
    
    This makes it possible use external password managers such as `pass` and
    `gpg` to store proxmox api passwords and connection tokens.  Normally, a
    script should be provided to run `pass`, `gpg`, or some other secrets
    management program with the correct arguments. The stdout should be in
    yaml format to specify one or more driver options.
    
    While here, add a better error messages if the proxmox_secret file is
    not found. This should help debug issues when the file is missing or
    just the wrong name or path is specified.
    
    Add var-naming[no-jinja] to the ansible-lint skip_list so we can
    dynamically set the api connection variables.
    
    * Add a convenience-script to export variables like a secrets scripts
    
    * Fix documentation to use correct debug variable
    
    * Remove obsolete condition check in block
    
    * Cleanup documentation for script usage
    
    ---------
    
    Co-authored-by: IamLunchbox <r.grieger@hotmail.com>

--- tests/test_proxmox_driver.py
+++ tests/test_proxmox_driver.py
@@ -37,7 +37,8 @@ def test_molecule_init_scenario(tmpdir):
 
 
 @pytest.mark.parametrize('scenario', [
-                           'default', 'by-name', 'by-vmid', 'cloud-init'])
+                           'default', 'by-name', 'by-vmid', 'cloud-init',
+                           'secrets-file', 'secrets-script'])
 def test_molecule_test(scenario):
     print('')
     testdir = pathlib.Path(__file__).resolve().parent
