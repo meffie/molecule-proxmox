3caa410b2494172608bdd583aa17bd4f74bd12f9
Michael Meffie <mmeffie@sinenomine.net>
2026-09-22 20:40:38 -0400

    setup: Add SPDX license specifier

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
     url='https://github.com/meffie/molecule-proxmox',
+    license='MIT',
     packages=[
         'molecule_proxmox',
         'molecule_proxmox.cookiecutter',
@@ -35,7 +36,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
